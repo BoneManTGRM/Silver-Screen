2dcf8d476425786942ed7d313058da8cb376c882
Reparodynamics <papadenico@yahoo.com>
2026-07-29 18:37:08 -0600

    fix: complete silver_screen/__init__.py exports for all modules + convenience imports

--- silver_screen/__init__.py
+++ silver_screen/__init__.py
@@ -2,6 +2,20 @@
 
 __version__ = "1.0.0"
 
-from . import science, tgrm
+from . import science, tgrm, script_engine, media, pipeline
 
-__all__ = ["science", "tgrm"]
+__all__ = [
+    "science",
+    "tgrm",
+    "script_engine",
+    "media",
+    "pipeline",
+    "SCIENCE",
+    "FIVE_LAWS",
+    "FORMATS",
+    "run_tgrm",
+]
+
+# Convenience re-exports
+from .science import SCIENCE, FIVE_LAWS, FORMATS
+from .tgrm import run_tgrm, detect_fractures, run_msil
